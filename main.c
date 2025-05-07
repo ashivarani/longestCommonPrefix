77fb5e748c23e6bfaa44ef56786fd1baa680f1d9
Shivarani Adla <83292756+ashivarani@users.noreply.github.com>
2025-05-07 12:26:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ char* longestCommonPrefix(char** strs, int strsSize) {
         char c = strs[0][i];
         for (int j = 1; j < strsSize; j++) {
             if (strs[j][i] != c || strs[j][i] == '\0') {
-                strs[0][i] = '\0';  // Truncate at mismatch
+                strs[0][i] = '\0';  
                 return strs[0];
             }
         }
